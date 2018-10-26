ad2e83e9b384f6f11288b58aff3cdbd06f9932b0
Kevin Risden <krisden@apache.org>
2018-10-26 15:13:45 +0000

    Add example jmeter test

--- jmeter/entrypoint.sh
+++ jmeter/entrypoint.sh
@@ -2,3 +2,10 @@
 
 set -eu
 
+COMPONENT="$1"
+
+rm -rf "/opt/jmeter_testresults/$COMPONENT"
+mkdir -p "/opt/jmeter_testresults/$COMPONENT"
+
+exec /opt/jmeter/bin/jmeter -n -l "/opt/jmeter_testresults/$COMPONENT/testresults.jtl" -e -o "/opt/jmeter_testresults/$COMPONENT/web" -t "/opt/jmeter_testplans/${COMPONENT}.jmx"
+
