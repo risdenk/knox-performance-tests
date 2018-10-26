60b58bbedc80c28a5aa0963d3b28153ccdb29a45
Kevin Risden <krisden@apache.org>
2018-10-26 13:42:27 +0000

    remove sleeps

--- hdfs/entrypoint.sh
+++ hdfs/entrypoint.sh
@@ -3,13 +3,11 @@
 set -eu
 set -x
 
-sleep 5
+rm -f /tmp/*.pid
 
 /opt/hadoop/bin/hdfs --config /opt/hadoop/etc/hadoop/ --daemon start namenode
 /opt/hadoop/bin/hdfs --config /opt/hadoop/etc/hadoop/ --daemon start secondarynamenode
 /opt/hadoop/bin/hdfs --config /opt/hadoop/etc/hadoop/ --daemon start datanode
-
-sleep 3
 
 tail -f /opt/hadoop/logs/*
 
