e3b2bebc0d3354460567df17005649a912f88881
Kevin Risden <krisden@apache.org>
2018-10-26 16:23:22 +0000

    Fix tar

--- jmeter/entrypoint.sh
+++ jmeter/entrypoint.sh
@@ -9,4 +9,4 @@ mkdir -p "/opt/jmeter_testresults/$COMPONENT"
 
 /opt/jmeter/bin/jmeter -n -l "/opt/jmeter_testresults/$COMPONENT/testresults.jtl" -e -o "/opt/jmeter_testresults/$COMPONENT/web" -t "/opt/jmeter_testplans/${COMPONENT}.jmx" >/opt/jmeter/run.log 2>&1
 
-cd /opt/ && tar -cOzf - jmeter_testresults
+cd /opt/ && tar -cOf - jmeter_testresults
