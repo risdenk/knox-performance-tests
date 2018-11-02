f8401c0a208761bab17664dc9ad45e974567ccf8
Kevin Risden <krisden@apache.org>
2018-11-02 15:15:12 -0400

    Make sure setup succeeds

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 set -eu
 
+docker-compose down -v --remove-orphans
 docker-compose build --pull
-docker-compose up -d --force-recreate --renew-anon-volumes knox hdfs hbase hive hive-binary
+docker-compose up -d --force-recreate --renew-anon-volumes --remove-orphans knox hdfs hbase hive hive-binary
